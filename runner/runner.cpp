1a08b5a3d7d1aac5439101db3f5ef059d1374abd
dfurle <denis.furletov@gmail.com>
2023-07-18 04:44:56 -0400

    fixed runner commented out lines

--- runner/runner.cpp
+++ runner/runner.cpp
@@ -23,37 +23,37 @@ void runner(input_raw_t in1_gmem[N_INPUT_1_1 * NUM_TRACKS], result_t out1_gmem[N
   // #pragma HLS ARRAY_RESHAPE variable=rot complete dim = 0
   // #pragma HLS ARRAY_RESHAPE variable=out1 complete dim = 0
 
-  out1_gmem[0] = in1_gmem[0];
+  // out1_gmem[0] = in1_gmem[0];
 
-  // MAIN_RUNNER:
-  // for(int trkNum = 0; trkNum < NUM_TRACKS; trkNum++){
-  //   // #pragma HLS PIPELINE off
-  //   #pragma HLS PIPELINE II=3
-  //   hls::vector<input_raw_t,N_INPUT_1_1> in1_loc;
+  MAIN_RUNNER:
+  for(int trkNum = 0; trkNum < NUM_TRACKS; trkNum++){
+    // #pragma HLS PIPELINE off
+    #pragma HLS PIPELINE II=3
+    hls::vector<input_raw_t,N_INPUT_1_1> in1_loc;
 
-  //   // input_raw_t buff[24];
-  //   // memcpy(buff, (const input_raw_t*) in1_gmem, 24 * sizeof(input_raw_t));
+    // input_raw_t buff[24];
+    // memcpy(buff, (const input_raw_t*) in1_gmem, 24 * sizeof(input_raw_t));
 
-  //   READ_DATA:
-  //   for(int i = 0; i < N_INPUT_1_1; i++){
-  //     #pragma HLS unroll
-  //     in1_loc[i] = in1_gmem[i + (N_INPUT_1_1 * trkNum)];
-  //     // in1_loc[i] = buff[i];
-  //   }
+    READ_DATA:
+    for(int i = 0; i < N_INPUT_1_1; i++){
+      #pragma HLS unroll
+      in1_loc[i] = in1_gmem[i + (N_INPUT_1_1 * trkNum)];
+      // in1_loc[i] = buff[i];
+    }
 
-  //   // hls::vector<layer1_t,N_INPUT_1_1> rot_loc;
-  //   // rotationKernel(in1_loc, rot_loc);
+    hls::vector<layer1_t,N_INPUT_1_1> rot_loc;
+    rotationKernel(in1_loc, rot_loc);
 
-  //   hls::vector<result_t,N_LAYER_8> out1_loc;
-  //   // NN::NNFakeOverlap(rot_loc, out1_loc);
-  //   out1_loc[0] = in1_loc[0];
+    hls::vector<result_t,N_LAYER_8> out1_loc;
+    NN::NNFakeOverlap(rot_loc, out1_loc);
+    // out1_loc[0] = in1_loc[0];
 
-  //   WRITE_DATA:
-  //   for(int i = 0; i < N_LAYER_8; i++){
-  //     #pragma HLS unroll
-  //     out1_gmem[i + (N_LAYER_8 * trkNum)] = out1_loc[i];
-  //   }
-  // }
+    WRITE_DATA:
+    for(int i = 0; i < N_LAYER_8; i++){
+      #pragma HLS unroll
+      out1_gmem[i + (N_LAYER_8 * trkNum)] = out1_loc[i];
+    }
+  }
 
   //  other
 
